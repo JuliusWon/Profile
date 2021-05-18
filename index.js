b2433434a909df1b1c7f3c4b04fa9488143ac000
JuliusWon <71460934+JuliusWon@users.noreply.github.com>
2021-05-18 13:52:57 -0400

    First draft

---
+++ index.js
@@ -0,0 +1,4 @@
+var urls = ["https://nourek.itch.io/","https://github.com/JuliusWon/","https://sidequestvr.com/app/1613/laser-arcade-vr","https://www.youtube.com/channel/UCpGiOJXgVeUbdEncJF9lNmw"]
+function OpenRes(number){
+	window.open(urls[number])
+}
